0a411e5f89412e3480433a48e91d25af7e5c0ef3
kadu-ymd <carlosepy@al.insper.edu.br>
2025-06-12 18:02:48 -0300

    Adicionando modelo de classificação de estampas

--- app/clothes_models.py
+++ app/clothes_models.py
@@ -9,24 +9,59 @@ from torchvision import transforms
 
 from .segmentation import cloth_segmentation_batch
 
+# Labels para cada classificação
+## Cor
 COLOR_LABELS = [
     'Beige', 'Black', 'Blue', 'Brown', 
     'Green', 'Grey', 'Multicolor', 
     'Orange', 'Pink', 'Purple', 
     'Red', 'White', 'Yellow',
 ]
-COLOR_IMG_SIZE = (64, 64)  # Tamanho da imagem de entrada para o modelo de cor
 
+## Categoria
+CATEGORY_LABELS = [
+    'Blouses', 'Dresses', 'Tops',
+    'Trousers', 'Blazers', 'Knitwear',
+    'Shirts', 'Coats', 'Jumpsuits',
+    'Jackets', 'Skirts', 'Sweaters',
+    'Vests', 'Cardigans', 'Shorts'
+]
+
+## Estampa
+DETAILS_LABELS = [
+    'Pattern', 'Floral', 'Solid', 
+    'Animal print', 'Checkers', 'Stripes'
+]
+
+# Tamanho da imagem de entrada para o modelo de cor e estampa
+COLOR_IMG_SIZE = (64, 64) 
+
+# Carregando os modelos
+## Modelo para classificação por cor
 color_model = torch.jit.load("./models/jit_model_color_66.pt")
 
+## Modelo para classificação por estampa
+details_model = torch.jit.load("./models/jit_model_detail_6.pt")
+
+## Modelo para classificação por categoria
+# category_model = torch.jit.load("./models/<nome_do_arquivo>.pt")
+
+# Criando objetos Compose para os modelos
+## Compose para modelo de classificação por cor
 color_transform = transforms.Compose([
   transforms.ToTensor(),
   transforms.Resize(COLOR_IMG_SIZE),
   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
 ])
 
-def apply_color_model(images: List[Tensor], device: str = "cpu") -> List[str]:
+## Compose para modelo de classificação por estampa
+details_transform = transforms.Compose([
+    transforms.Resize(COLOR_IMG_SIZE),
+    transforms.ToTensor()
+])
 
+
+def apply_color_model(images: List[Tensor], device: str = "cpu") -> List[str]:
     color_model.to(device).eval()
     
     batch_tensor = torch.stack([color_transform(img) for img in images]).to(device)
@@ -36,32 +71,28 @@ def apply_color_model(images: List[Tensor], device: str = "cpu") -> List[str]:
     
     return [COLOR_LABELS[color] for color in pred_colors]
 
-CATEGORY_LABELS = [
-    'Blouses', 'Dresses', 'Tops',
-    'Trousers', 'Blazers', 'Knitwear',
-    'Shirts', 'Coats', 'Jumpsuits',
-    'Jackets', 'Skirts', 'Sweaters',
-    'Vests', 'Cardigans', 'Shorts'
-]
 
 def apply_category_model(images: List[Tensor], device: str = "cpu") -> List[str]:
     # Placeholder for category model application
     return [np.random.choice(CATEGORY_LABELS) for _ in images]
 
-DETAILS_LABELS = [
-    'Pattern', 'Floral', 'Solid', 
-    'Animal print', 'Checkers', 'Stripes'
-]
 
 def apply_detail_model(images: List[Tensor], device: str = "cpu") -> List[str]:
-    # Placeholder for detail model application
-    return [np.random.choice(DETAILS_LABELS) for _ in images]
+    details_model.to(device).eval()
+
+    batch_tensor = torch.stack([details_transform(img) for img in images]).to(device)
+    with torch.no_grad():
+        details_results = details_model(batch_tensor).cpu().numpy()
+        pred_details = np.argmax(details_results, axis=1)
+
+    return [DETAILS_LABELS[detail] for detail in pred_details]
+
 
 def apply_cloth_models(images:List[np.ndarray], size:float=1, device:str="cpu") -> List[Dict[str, str]]:
-    print(f"preprocessing images...")
+    print(f"Preprocessing images...")
     preprocessed_images = cloth_segmentation_batch(images, size, device)
     
-    print(f"predicting...")
+    print(f"Predicting...")
     pred_colors = apply_color_model(preprocessed_images, device)
     pred_categories = apply_category_model(preprocessed_images, device)
     pred_details = apply_detail_model(preprocessed_images, device)
